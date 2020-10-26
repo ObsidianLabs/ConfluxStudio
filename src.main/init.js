636cf9b8663f8895c3320b6aaba82c83b71b21ff
Phil <qft.gtr@gmail.com>
2020-10-26 18:05:48 +0800

    Update init.js

--- src.main/init.js
+++ src.main/init.js
@@ -9,7 +9,7 @@ let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, pr
 module.exports = function () {
   ipcChannel = new IpcChannel()
   keypairManager = new KeypairManager(process.env.BUILD)
-  autoUpdate = new AutoUpdate('https://app.eosstudio.io/api/v1/check-update-conflux/')
+  autoUpdate = new AutoUpdate('https://app.obsidians.io/api/v1/check-update/conflux/')
   compilerManager = new CompilerManager()
   instanceManager = new InstanceManager()
   projectChannel = new ProjectChannel()
