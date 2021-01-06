9ae3bbd8acfff86b811e261c5219c4701b12ef90
Leon <leon390422914@gmail.com>
2021-01-06 17:24:51 +0800

    feat: add cdn for react/react-dom/monaco

--- src/components/Routes/Project.js
+++ src/components/Routes/Project.js
@@ -4,24 +4,6 @@ import platform from '@obsidians/platform'
 import { connect } from '@obsidians/redux'
 import Project from '@obsidians/project'
 
-window.MonacoEnvironment = {
-  getWorkerUrl: function (moduleId, label) {
-    if (label === 'json') {
-      return './json.worker.js';
-    }
-    if (label === 'css') {
-      return './css.worker.js';
-    }
-    if (label === 'html') {
-      return './html.worker.js';
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return './ts.worker.js';
-    }
-    return './editor.worker.js';
-  }
-}
-
 class ProjectWithProps extends PureComponent {
   async componentDidMount () {
     this.props.cacheLifecycles.didRecover(() => {
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import '@obsidians/platform'
+import $loadjs from 'loadjs'
 
-import platform from '@obsidians/platform'
-import fileOps from '@obsidians/file-ops'
-
-import './scss/index.scss'
-
-import * as serviceWorker from './serviceWorker'
-import App from './App'
-
-if (platform.isDesktop) {
-  fileOps.fsType = 'electron'
+if (!process.env.CDN) {
+  import('./react')
 } else {
-  fileOps.fsType = 'web'
+  // Fetch files in parallel and load them in series
+  $loadjs([
+    'https://cdnjs.cloudflare.com/ajax/libs/react/16.12.0/umd/react.production.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.12.0/umd/react-dom.production.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/loader.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.nls.js',
+  ], 'bundle', {
+    async: false,
+  })
+
+  window.require = { paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs' } }
+
+  window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/typeface-open-sans@1.1.13/index.min.css">`
+  window.document.head.innerHTML += `<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-9ZfPnbegQSumzaE7mks2IYgHoayLtuto3AS6ieArECeaR8nCfliJVuLh/GaQ1gyM" crossorigin="anonymous">`
+  window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hack-font/3.3.0/web/hack.min.css" integrity="sha512-XgCw4Srl8lC1ECwcaHwAU0WnxQwHkqmInzg9wJLtGB7DRuMaXPuK2k9WJ2AwRDGdrgK9eJpZl2hUlLi2WQssmw==" crossorigin="anonymous" />`
+  window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min/vs/editor/editor.main.min.css" integrity="sha512-9uX8QlyL0SosYXO3oNqyiXdnmhtWk22wutqEzGR53Bezc+yqYVvFukBAOW97fPx/3Dxdul77zW27GwHRzdYfMg==" crossorigin="anonymous" />`
+
+  $loadjs.ready('bundle', () => {
+    import('./react')
+  })
 }
-
-import('./scss/fonts/open_sans.css')
-import('./scss/fonts/hack.css')
-import('@fortawesome/fontawesome-pro/js/all')
-
-document.title = process.env.PROJECT_NAME
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
