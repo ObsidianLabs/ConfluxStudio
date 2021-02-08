90b7156200daa418faa8fdf55f4ea8e7e3cdadd7
Phil <qft.gtr@gmail.com>
2021-02-08 15:05:30 +0800

    use networks from @obsidians/sdk

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,62 +2,26 @@ import React, { PureComponent } from 'react'
 
 import { connect } from '@obsidians/redux'
 
-import platform from '@obsidians/platform'
+import { networks } from '@obsidians/sdk'
 import headerActions, { Header, NavGuard } from '@obsidians/header'
 import { networkManager } from '@obsidians/network'
 import { actions } from '@obsidians/workspace'
 
 import { List } from 'immutable'
 
-const networkList = [
-  {
-    id: 'testnet',
-    group: 'testnet',
-    name: 'Testnet',
-    fullName: 'Conflux Testnet',
-    icon: 'fas fa-vial',
-    notification: 'Switched to <b>Conflux Testnet</b> network.',
-    url: 'https://test.confluxrpc.org',
-    chainId: 1,
-    explorer: 'https://testnet.confluxscan.io/v1',
-  },
-  {
-    id: 'tethys',
-    group: 'mainnet',
-    name: 'Tethys',
-    fullName: 'Conflux Tethys',
-    icon: 'fas fa-globe',
-    notification: 'Switched to <b>Conflux Tethys</b> network.',
-    url: 'https://main.confluxrpc.org',
-    chainId: 1029,
-    explorer: 'https://confluxscan.io/v1',
-  }
-]
-if (platform.isDesktop) {
-  networkList.unshift({
-    id: 'dev',
-    group: 'default',
-    name: 'Development',
-    fullName: 'Development Network',
-    icon: 'fas fa-laptop-code',
-    notification: 'Switched to <b>Development</b> network.',
-    url: 'http://localhost:12537',
-    chainId: 0,
-  })
-}
-const networks = List(networkList)
+const networkList = List(networks)
 
 class HeaderWithRedux extends PureComponent {
   componentDidMount () {
     actions.history = this.props.history
     headerActions.history = this.props.history
     if (!networkManager.network) {
-      networkManager.setNetwork(networks.get(0))
+      networkManager.setNetwork(networkList.get(0))
     }
     this.navGuard = new NavGuard(this.props.history)
   }
 
-  networkList = networksByGroup => {
+  groupedNetworks = networksByGroup => {
     const networkList = []
     const groups = networksByGroup.toJS()
     const keys = Object.keys(groups)
@@ -80,9 +44,9 @@ class HeaderWithRedux extends PureComponent {
 
     const selectedProject = projects.get('selected')?.toJS() || {}
 
-    const networkGroups = networks.groupBy(n => n.group)
-    const networkList = this.networkList(networkGroups)
-    const selectedNetwork = networks.find(n => n.id === network) || {}
+    const networkGroups = networkList.groupBy(n => n.group)
+    const groupedNetworks = this.groupedNetworks(networkGroups)
+    const selectedNetwork = networkList.find(n => n.id === network) || {}
 
     const starred = accounts.getIn([network, 'accounts'])?.toJS() || []
     const selectedContract = contracts.getIn([network, 'selected']) || ''
@@ -97,7 +61,7 @@ class HeaderWithRedux extends PureComponent {
         selectedAccount={selectedAccount}
         starred={starred}
         network={selectedNetwork}
-        networkList={networkList}
+        networkList={groupedNetworks}
       />
     )
   }
