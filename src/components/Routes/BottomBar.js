fa9729c8c587145a136e4b179bad459fbad76de4
Phil Li <qft.gtr@gmail.com>
2021-05-19 13:53:49 +0800

    Update BottomBar.js

--- src/components/Routes/BottomBar.js
+++ src/components/Routes/BottomBar.js
@@ -13,7 +13,11 @@ function BottomBarWithProps ({ network, queue, uiState }) {
   }
   return (
     <BottomBar
-      chains={['dev', 'testnet', 'mainnet']}
+      chains={[
+        { key: 'dev', text: 'Local develop networks', filter: key => key.startsWith('0x') },
+        { key: 'testnet', text: 'Testnet', filter: key => key.startsWith('cfxtest:') },
+        { key: 'mainnet', text: 'Tethys mainnet', filter: key => key.startsWith('cfx:'), network: 'tethys' }
+      ]}
       txs={txs}
     />
   )
