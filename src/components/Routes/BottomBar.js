8018a743b6f0077855d40f80d63716c7b51c97bd
Phil Li <qft.gtr@gmail.com>
2021-06-15 22:22:13 -0700

    Update BottomBar.js

--- src/components/Routes/BottomBar.js
+++ src/components/Routes/BottomBar.js
@@ -1,26 +1,12 @@
-import React from 'react'
-
-import { connect } from '@obsidians/redux'
 import BottomBar from '@obsidians/bottombar'
 
-function BottomBarWithProps ({ network, queue, uiState }) {
-  const localNetwork = uiState.get('localNetwork')
-  let txs
-  if (network !== 'dev') {
-    txs = queue.getIn([network, 'txs'])
-  } else if (localNetwork && localNetwork.lifecycle === 'started') {
-    txs = queue.getIn([localNetwork.params.id, 'txs'])
-  }
-  return (
-    <BottomBar
-      chains={[
-        { key: 'dev', text: 'Local develop networks', filter: key => key.startsWith('0x') },
-        { key: 'testnet', text: 'Testnet', filter: key => key.startsWith('cfxtest:') },
-        { key: 'mainnet', text: 'Tethys mainnet', filter: key => key.startsWith('cfx:'), network: 'tethys' }
-      ]}
-      txs={txs}
-    />
-  )
+BottomBar.defaultProps = {
+  mnemonic: false,
+  chains: [
+    { key: 'dev', text: 'Local develop networks', filter: key => key.startsWith('0x') },
+    { key: 'testnet', text: 'Testnet', filter: key => key.startsWith('cfxtest:') },
+    { key: 'mainnet', text: 'Tethys mainnet', filter: key => key.startsWith('cfx:'), network: 'tethys' }
+  ]
 }
 
-export default connect(['queue', 'network', 'uiState'])(BottomBarWithProps)
+export default BottomBar
