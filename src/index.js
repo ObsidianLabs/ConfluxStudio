3b1911fe9aecec365033403bac66ebe981e41d22
Phil Li <qft.gtr@gmail.com>
2021-06-27 20:31:51 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import '@obsidians/platform'
 import $loadjs from 'loadjs'
 
 if (!process.env.CDN) {
-  $loadjs([
-    'https://solc-bin.ethereum.org/bin/list.js',
-  ], 'bundle', {
-    async: false,
-  })
+  import('./react')
 } else {
   // Fetch files in parallel and load them in series
   $loadjs([
     'https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.production.min.js',
     'https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.production.min.js',
-    'https://solc-bin.ethereum.org/bin/list.js',
     'https://cdn.jsdelivr.net/npm/monaco-editor@0.21.2/min/vs/loader.js',
     'https://cdn.jsdelivr.net/npm/monaco-editor@0.21.2/min/vs/editor/editor.main.js',
     'https://cdn.jsdelivr.net/npm/monaco-editor@0.21.2/min/vs/editor/editor.main.nls.js',
@@ -26,8 +21,8 @@ if (!process.env.CDN) {
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-9ZfPnbegQSumzaE7mks2IYgHoayLtuto3AS6ieArECeaR8nCfliJVuLh/GaQ1gyM" crossorigin="anonymous">`
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack.css" />`
   window.document.head.innerHTML += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/monaco-editor@0.21.2/min/vs/editor/editor.main.css" />`
-}
 
-$loadjs.ready('bundle', () => {
-  import('./react')
-})
+  $loadjs.ready('bundle', () => {
+    import('./react')
+  })
+}
